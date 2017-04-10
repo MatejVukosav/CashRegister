ce29e1f22a5913f4f125cd849b040dc7d816b364
MatejVukosav <146@gmail.com>
2017-04-10 17:49:14 +0200

    added cash class

---
+++ CashRegister/Cash.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashRegister
+{
+    class Cash
+    {
+        List<Bill> bills;
+
+        public Cash()
+        {
+            bills = new List<Bill>();
+        }
+
+        public void getReport( )
+        {
+            //dohvatit broj svih racuna
+            //ukupnu vrijednost prodane robe
+            //popis svih racuna
+        }
+
+        private float getSumBillsValue(List<Bill> bills)
+        {
+            float value=0;
+            foreach(Bill bill in bills)
+            {
+                value += bill.getSumPrice();
+            }
+            return value;
+        }
+
+        private void printAllBills(List<Bill> bills)
+        {
+            foreach (Bill bill in bills)
+            {
+               //isprintaj racun Vrijeme izdavanja - Broj artikala - iznos racuna
+            }
+        }
+
+    }
+}
